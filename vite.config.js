074bdc3b0fded0bf8e5038b55e0013f2560628f5
Alikhan Beisenbayev <rkoenigseggagera@gmail.com>
2025-04-07 19:46:33 +0200

    structure and base for project

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import fs from 'fs';
+
+const pagesDir = resolve(__dirname, 'src/pages');
+const htmlFiles = fs.readdirSync(pagesDir).filter(file => file.endsWith('.html'));
+
+const input = htmlFiles.reduce((entries, file) => {
+  const name = file.replace('.html', '');
+  entries[name] = resolve(pagesDir, file);
+  return entries;
+}, {});
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input,
+    }
+  }
+});
