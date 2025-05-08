187c1c55be5e3070f9c2e05cb9555bb77c2ae4ae
2018PAN <1365863356@qq.com>
2025-05-08 22:25:53 +0200

    render GeoJSON data to the map

--- src/js/gamemodeOne.js
+++ src/js/gamemodeOne.js
@@ -1,12 +1,59 @@
 var map = L.map('map', {
-    
-    attributionControl: false
-}).setView([20, 0], 2);
 
-L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png', {
+    attributionControl: false
+}).setView([10, 0], 2);
+
+const tile = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png', {
     maxZoom: 20
 }).addTo(map);
 
-setTimeout(function() {
-    map.invalidateSize();
-}, 100);
+
+const container = document.getElementById('map');
+let gameActive = false;
+
+
+fetch('../../public/assets/data/WorldBaseMap/Countries_Info.geojson')
+    .then(res => res.json())
+    .then(geojsonData => {
+        const defaultStyle = {
+            weight: 0.5,
+            color: '#342F33',
+            fillColor: '#FFEEE2',
+            fillOpacity: 0.9,
+        };
+
+        L.geoJSON(geojsonData, {
+            style: defaultStyle,
+
+            onEachFeature: (feature, layer) => {
+                const txt = feature.properties.ADMIN;
+                layer.bindTooltip(txt, {
+                    sticky: true,
+                    direction: "top",
+                    opacity: 0.9
+                });
+
+
+                layer.on('mouseover', function (e) {
+                    if (!gameActive || !clickedCountries.has(feature.properties.ADMIN)) {
+                        this.setStyle({ fillOpacity: 1, weight: 1.5 });
+                    }
+                });
+                layer.on('mouseout', function (e) {
+                    if (!gameActive || !clickedCountries.has(feature.properties.ADMIN)) {
+                        this.setStyle({ fillOpacity: 0.9, weight: 0.5 });
+                    }
+                });
+            }
+        }).addTo(map);
+    })
+    .catch(err => console.error('failed to load geojson', err));
+
+
+// make sure the map can be rendered after the page is loaded
+if ('ResizeObserver' in window) {
+    const ro = new ResizeObserver(() => {
+        map.invalidateSize();
+    });
+    ro.observe(container);
+}
