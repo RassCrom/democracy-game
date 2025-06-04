fc3d519e6b590752859fb5073848b7bff6523336
Alikhan Beisenbayev <rkoenigseggagera@gmail.com>
2025-06-04 17:48:51 +0200

    fixed footer to be at the bottom always

--- src/js/main.js
+++ src/js/main.js
@@ -5,17 +5,25 @@ const LEFT_LINKS = ['countrybase', 'statistics', 'about'];
 const RIGHT_LINKS = ['setting', 'educational', 'tutorial'];
 const ALL_LINKS = [...LEFT_LINKS, ...RIGHT_LINKS];
 
+const wrapper = document.createElement('div');
+wrapper.classList.add('wrapper');
+document.body.prepend(wrapper);
+
+const mains = document.getElementsByTagName('main')[0];
+
 const activeNavUrl = window.location.pathname;
 let activeNav = activeNavUrl.split('/').slice(-1)
 activeNav[0] = activeNav[0].replace(/\.html$/, '')
 activeNav = activeNav[0]
 
 const header = new Header();
-document.body.prepend(header.render());
+wrapper.append(header.render());
+
+wrapper.append(mains);
 
 const footer = new Footer();
 if (ALL_LINKS.includes(activeNav)) {
-    document.body.append(footer.render());
+    wrapper.append(footer.render());
 }
 
 ALL_LINKS.forEach((link) => {
