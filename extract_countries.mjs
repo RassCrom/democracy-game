9a73dfd68122c8ab8b41bf86629f47c891c43a92
Alikhan Beisenbayev <rkoenigseggagera@gmail.com>
2025-06-10 12:11:35 +0200

    fixed paths

--- extract_countries.mjs
+++ extract_countries.mjs
@@ -1,6 +1,6 @@
 import { readFileSync, writeFileSync } from 'fs'
 
-const geojson = JSON.parse(readFileSync('./public/assets/countries_politicalsystem/Countries_Info.geojson', 'utf8'))
+const geojson = JSON.parse(readFileSync('/democracy-game/assets/countries_politicalsystem/Countries_Info.geojson', 'utf8'))
 
 const result = geojson.features.map(f => ({
 	name: f.properties.NAME,
@@ -9,5 +9,5 @@ const result = geojson.features.map(f => ({
 	iso_a3: f.properties.ISO_A3
 }))
 
-writeFileSync('./public/assets/countries_politicalsystem/countries_simple.json', JSON.stringify(result, null, 2), 'utf8')
+writeFileSync('/democracy-game/assets/countries_politicalsystem/countries_simple.json', JSON.stringify(result, null, 2), 'utf8')
 console.log('countries_simple.json created!') 
